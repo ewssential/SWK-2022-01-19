12e01ee17dbd68c2bcedd6c429c432632342e8b1
Eike Wilke <eike.wilke@mailbox.org>
2022-01-20 20:03:34 +0100

    define solver

--- solution.py
+++ solution.py
@@ -1,8 +1,26 @@
 import unittest
+ 
+def solver(participants):
+    return participants
 
 class TestStringMethods(unittest.TestCase):
 
+
+    def test_solver(self):
+        participants = {}
+        result = solver(participants)
+        self.assertIsNotNone(result)
+        
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
+
 if __name__ == '__main__':
     unittest.main()
+
+
+
+## Ablauf
+# csv einlesen
+# optimierung
+    # -> participant --> rollenzuordnung
+# ausgabe
