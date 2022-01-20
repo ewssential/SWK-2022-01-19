6868676a95834d595239497b290abaa43f9a4d71
Eike Wilke <eike.wilke@mailbox.org>
2022-01-20 20:03:56 +0100

    cleanup

--- solution.py
+++ solution.py
@@ -5,14 +5,10 @@ def solver(participants):
 
 class TestStringMethods(unittest.TestCase):
 
-
     def test_solver(self):
         participants = {}
         result = solver(participants)
         self.assertIsNotNone(result)
-        
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
 
 if __name__ == '__main__':
     unittest.main()
