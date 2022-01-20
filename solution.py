00dbf21868b9f5b3b2e9bec87f0e692b8f3ca645
luebecko <luebecko@github.com>
2022-01-20 20:59:01 +0100

    saaldiener

--- solution.py
+++ solution.py
@@ -1,8 +1,24 @@
 import unittest
  
 def solver(participants):
-    zeitnehmer = next(filter(lambda x: x['zeitnehmer'] == True, participants))
-    return {'zeitnehmer': zeitnehmer['number']}
+    zeitnehmer = list(filter(lambda x: x['zeitnehmer'] == True, participants))
+    saaldiener = list(filter(lambda x: x['saaldiener'] == True, participants))
+    if (len(zeitnehmer) == 0):
+        return {
+            'zeitnehmer': None,
+            'saaldiener': saaldiener[0]['number']
+            }
+
+    if (len(saaldiener) == 0):
+        return {
+        'zeitnehmer': zeitnehmer[0]['number'],
+        'saaldiener': None
+            }
+
+    return {
+        'zeitnehmer': zeitnehmer[0]['number'],
+        'saaldiener': saaldiener[0]['number']
+        }
 
 class TestStringMethods(unittest.TestCase):
 
@@ -10,7 +26,8 @@ class TestStringMethods(unittest.TestCase):
         participants = [{
             'name': 'Eins',
             'number': 1,
-            'zeitnehmer': True
+            'zeitnehmer': True,
+            'saaldiener': False
         }]
         result = solver(participants)
         self.assertIsNotNone(result)
@@ -19,7 +36,8 @@ class TestStringMethods(unittest.TestCase):
         participants = [{
             'name': 'Eins',
             'number': 1,
-            'zeitnehmer': True
+            'zeitnehmer': True,
+            'saaldiener': False
         }]
         result = solver(participants)
         self.assertEqual(result['zeitnehmer'], 1)
@@ -28,27 +46,61 @@ class TestStringMethods(unittest.TestCase):
         participants = [{
             'name': 'Zwei',
             'number': 2,
-            'zeitnehmer': True
+            'zeitnehmer': True,
+            'saaldiener': False
         },{
             'name': 'Eins',
             'number': 1,
-            'zeitnehmer': True
+            'zeitnehmer': True,
+            'saaldiener': False
         }]
         result = solver(participants)
         self.assertEqual(result['zeitnehmer'], 2)
 
-    def test_two_zeitnehmer(self):
+    def test_two_zeitnehmer_with_invalid_zeitnehmer(self):
         participants = [{
             'name': 'Zwei',
             'number': 2,
-            'zeitnehmer': False
+            'zeitnehmer': False,
+            'saaldiener': False
         },{
             'name': 'Eins',
             'number': 1,
-            'zeitnehmer': True
+            'zeitnehmer': True,
+            'saaldiener': False
         }]
         result = solver(participants)
         self.assertEqual(result['zeitnehmer'], 1)
+
+    def test_just_one_saaldiener(self):
+        participants = [{
+            'name': 'Zwei',
+            'number': 3,
+            'zeitnehmer': False,
+            'saaldiener': True
+        },{
+            'name': 'Eins',
+            'number': 4,
+            'zeitnehmer': True,
+            'saaldiener': False
+        }]
+        result = solver(participants)
+        self.assertEqual(result['saaldiener'], 3)
+
+    def test_just_one_saaldiener(self):
+        participants = [{
+            'name': 'Zwei',
+            'number': 3,
+            'zeitnehmer': True,
+            'saaldiener': False
+        },{
+            'name': 'Eins',
+            'number': 4,
+            'zeitnehmer': True,
+            'saaldiener': True
+        }]
+        result = solver(participants)
+        self.assertEqual(result['saaldiener'], 4)
 
 if __name__ == '__main__':
     unittest.main()
